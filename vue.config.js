7ef20ec13cad71273f6e9014860eed5fcfa7989b
eladtt <elad.tanami@gmail.com>
2021-02-02 15:59:59 +0200

    gitignor

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer:{
         proxy:{
             '/api':{
-                target:'http://localhost:8082',
+                target:'http://localhost:8081',
                 changeOrigin: true,
             }
         }
